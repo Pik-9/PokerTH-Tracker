a644181453764bac205a4bfae30f0c5d391baf80
Daniel Steinhauer <d.steinhauer@mailbox.org>
2015-12-31 16:07:38 +0100

    Added license information.

--- Lang/PokerTH_tracker_de.ts
+++ Lang/PokerTH_tracker_de.ts
@@ -4,7 +4,7 @@
 <context>
     <name>LeftPart</name>
     <message>
-        <location filename="../mainwindow.cpp" line="33"/>
+        <location filename="../mainwindow.cpp" line="55"/>
         <source>Search...</source>
         <translation>Durchsuchen...</translation>
     </message>
@@ -12,98 +12,103 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.cpp" line="144"/>
-        <location filename="../mainwindow.cpp" line="335"/>
+        <location filename="../mainwindow.cpp" line="166"/>
+        <location filename="../mainwindow.cpp" line="360"/>
         <source>PokerTH Tracker</source>
         <translation>PokerTH Tracker</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="152"/>
+        <location filename="../mainwindow.cpp" line="174"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="153"/>
+        <location filename="../mainwindow.cpp" line="175"/>
         <source>&amp;Refresh</source>
         <translation>&amp;Aktualisieren</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="154"/>
+        <location filename="../mainwindow.cpp" line="176"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="157"/>
+        <location filename="../mainwindow.cpp" line="179"/>
         <source>&amp;Help</source>
         <translation>&amp;Hilfe</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="158"/>
+        <location filename="../mainwindow.cpp" line="180"/>
         <source>About Qt</source>
         <translation>Über Qt</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="159"/>
+        <location filename="../mainwindow.cpp" line="181"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="320"/>
+        <location filename="../mainwindow.cpp" line="342"/>
         <source>Opened %1 files from %2</source>
         <translation>%1 Dateien aus %2 geöffnet</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="335"/>
+        <location filename="../mainwindow.cpp" line="357"/>
         <source>PokerTH Tracker - A simple poker statistics tracker for PokerTH
-(C) 2015-2016 Daniel Steinhauer (9 of Spades)</source>
+</source>
         <translation>PokerTH Tracker - Ein einfaches Statistik Analyse Programm für PokerTH
-(C) 2015-2016 Daniel Steinhauer (9 of Spades)</translation>
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="359"/>
+        <source>This software is published under the termns of the GNU General Public License Version 3.</source>
+        <translation>Dieses Programm ist unter der GNU General Public License in Version 3 veröffentlicht.</translation>
     </message>
 </context>
 <context>
     <name>RightPart</name>
     <message>
-        <location filename="../mainwindow.cpp" line="73"/>
+        <location filename="../mainwindow.cpp" line="95"/>
         <source>Observed hands:</source>
         <translation>Beobachtete Hände:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="74"/>
+        <location filename="../mainwindow.cpp" line="96"/>
         <source>VP$IP:</source>
         <translation>VP$IP:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="75"/>
+        <location filename="../mainwindow.cpp" line="97"/>
         <source>Preflop Raise:</source>
         <translation>Preflop Erhöhung:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="76"/>
+        <location filename="../mainwindow.cpp" line="98"/>
         <source>Flop Aggression Factor:</source>
         <translation>Flop Aggressionsfaktor:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="77"/>
+        <location filename="../mainwindow.cpp" line="99"/>
         <source>Continuation bet:</source>
         <translation>Continuation Bet:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="78"/>
+        <location filename="../mainwindow.cpp" line="100"/>
         <source>Turn Aggression Factor:</source>
         <translation>Turn Aggressionsfaktor:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="79"/>
+        <location filename="../mainwindow.cpp" line="101"/>
         <source>River Aggression Factor:</source>
         <translation>River Aggressionsfaktor:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="80"/>
+        <location filename="../mainwindow.cpp" line="102"/>
         <source>Went to Showdown:</source>
         <translation>Bis in den Showdown gegangen:</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="81"/>
+        <location filename="../mainwindow.cpp" line="103"/>
         <source>Won Showdown:</source>
         <translation>Showdown gewonnen:</translation>
     </message>
