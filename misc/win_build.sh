6c0b758fda6927f081690b3ed7205e00e872bc56
Daniel Steinhauer <d.steinhauer@mailbox.org>
2016-02-06 00:15:01 +0100

    Added a simple bash script to make the Windows release.

---
+++ misc/win_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Daniel Steinhauer (9 of Spades)
+# This is just a quick and dirty script to compile this app for Windows.
+
+cd $(dirname $0)
+rm -rf ../win_build
+mkdir ../win_build
+cd ../win_build
+
+lrelease ../PokerTH_tracker.pro
+i686-w64-mingw32.static-qmake-qt5 ../PokerTH_tracker.pro
+make
+zip -j PokerTH-Tracker_win32.zip ../Lang/*.qm ../Icons/*.png release/PokerTH_tracker.exe
+rm ../Lang/*.qm
